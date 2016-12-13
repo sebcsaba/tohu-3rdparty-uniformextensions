ea25b90d952be0704445de145626a0e6bd20f61e
sebcsaba <sebcsaba@gmail.com>
2016-12-13 22:17:58 +0100

    add set_limits

---
+++ set_limits.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Form edit oldalon, Scripts lapon az elso dobozba ird be ezt, es ez tovabbvezet a teljes folyamaton:
+ * require_once JPATH_ROOT.DS.'thirdparty/uniformextensions/set_limits.php';help($formId);
+ */
+
+function help($formId) {
+    echo $formId;
+}
+
+?>
